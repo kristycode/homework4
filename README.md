38c1de9a41d7219f1b62d5b3f92f6fa8a1c010e4
Kristy Farmer <kristyfarmerschool@gmail.com>
2020-02-19 23:40:45 -0600

    I'm just going to upload what I have so far... I cannot get thehigh scores list to workgit add -A

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # homework4
 My repo for my 4th class homework!
+
+My script includes an object called "questions," wherein the properties are the sets of questions, options and the answer.
---
+++ assets/script.js
@@ -0,0 +1,180 @@
+
+// home page vars //
+const startContainer = document.getElementById("startContainer");
+const startButton = document.getElementById("startButton");
+const playAgainButton =  document.getElementById("playAgainButton");
+const highScoresButton =  document.getElementById("highScoresButton");
+// quiz //
+const quizContainer = document.getElementById("quizContainer");
+const optionA = document.getElementById("a");
+const optionB = document.getElementById("b");
+const optionC = document.getElementById("c");
+const optionD =  document.getElementById("d");
+// final score page //
+const answerResultContainer = document.getElementById("answerResultContainer");
+const scoreResult = document.getElementById("scoreResult");
+const scoreAmount = document.getElementById("scoreAmount");
+// score submission //
+const username = document.getElementById("username");
+const saveScoreButton = document.getElementById("saveScoreButton");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+// high scores list page //
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+const highScoresList = document.getElementById("highScoresList");
+const highScoreContainer = document.getElementById("highScoreContainer");
+
+
+// the questions //
+let questions = [
+    {
+    question: "Commonly used data types do NOT include:",
+    optionA : "a. strings",
+    optionB : "b. booleans",
+    optionC : "c. alerts",
+    optionD : "d. numbers",
+    answer : "c"
+},{
+    question : "The condition in an if/else statement is enclosed within ______:",
+    optionA : "a. quotes",
+    optionB : "b. curly brackets",
+    optionC : "c. parenthesis",
+    optionD : "d. square brackets",
+    answer : "c"
+},{
+    question : "Arrays in Javascript can be used to store ______:",
+    optionA : "a. numbers and strings",
+    optionB : "b. other arrays",
+    optionC : "c. booleans",
+    optionD : "d. all of the above",
+    answer : "d"
+},{
+    question : "String values must be enclosed within _____ when being assigned to variables:",
+    optionA : "a. commas",
+    optionB : "b. curly brackets",
+    optionC : "c. quotes",
+    optionD : "d. parenthesis",
+    answer : "c"
+},{
+    question : "A very useful tool used during development and debugging for printing content to the debugger is:",
+    optionA : "a. JavaScript",
+    optionB : "b. terminal/bash",
+    optionC : "c. for loops",
+    optionD : "d. console.log",
+    answer : "d"
+}]
+
+// variables //
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let score = 0;
+
+
+//rendering the question //
+function renderQuestion(){
+    let q = questions[runningQuestion];
+    question.innerHTML = "<p>" + q.question + "</p>";
+    optionA.innerHTML = q.optionA;
+    optionB.innerHTML = q.optionB;
+    optionC.innerHTML = q.optionC;
+    optionD.innerHTML = q.optionD;
+}
+
+startButton.addEventListener("click", startQuiz);
+// start quiz //
+function startQuiz (){
+    startContainer.style.display = "none";
+    renderQuestion();
+    quizContainer.style.display = "block";
+    // start my timer function here when i make it //
+
+}
+
+//check answers//
+function checkAnswer(answer){
+    if (answer == questions[runningQuestion].answer){
+        //answer is correct
+        score++;
+        // change the answer alert div
+        answerResult.innerText = "last answer was correct";
+    }
+    else{
+        answerResult.innerText = "last answer was wrong";
+    }
+    // run renderQuestion function again but i can't get the setTimeout to work on it? //
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }
+    else{
+        scoreCalculate();
+    }
+}
+
+//show the final score//
+
+function scoreCalculate(){
+    quizContainer.style.display = "none";
+    scoreResultContainer.style.display = "block";
+
+    // calculate and save the score //
+    const scorePercent = Math.round(100 * score/questions.length);
+    finalScore.innerText = scorePercent;
+    localStorage.setItem("mostRecentScore", scorePercent);
+
+}
+
+//save score functions//
+username.addEventListener("keyup", () => {
+    saveScoreButton.disabled = !username.value;
+});
+
+saveHighScore = e => {
+    e.preventDefault();
+    
+    const scoreTracker = {
+        name: username.value,
+        score: mostRecentScore
+    };
+
+    highScores.push(scoreTracker);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    renderScores()
+    console.log(highScores);
+
+};
+
+highScoresList.innerHTML = highScores
+function renderScores() {
+    for (var i =0; i < highScores.length; i++) {
+        var newScoreAdded = highScores[i];
+
+        var li = document.createElement("li");
+        li.textContent = newScoreAdded;
+        highScoresList.appendChild(li);
+} 
+};
+
+
+highScoresButton.addEventListener("click", displayHighScores);
+function displayHighScores (){
+    startContainer.style.display = "none";
+    scoreResultContainer.style.display = "none";
+    highScoreContainer.style.display = "block";
+    answerResult.innerText = "";
+}
+
+playAgainButton.addEventListener("click", startNewQuiz);
+function startNewQuiz (){
+    highScoreContainer.style.display= "none";
+    startContainer.style.display = "none";
+    scoreResultContainer.style.display = "none";
+    runningQuestion = 0;
+    score = 0;
+    answerResult.innerText = "";
+    renderQuestion();
+    quizContainer.style.display = "block";
+    // start my timer function here when i make it //
+
+}
