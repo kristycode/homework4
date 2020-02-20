e507f61621ac2f217178136ac5253a63c861ad56
Kristy Farmer <kristyfarmerschool@gmail.com>
2020-02-20 02:42:43 -0600

    I've updated my README and gotten the High Scores list to kind of work. I have just the timer left to do.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # homework4
 My repo for my 4th class homework!
 
-My script includes an object called "questions," wherein the properties are the sets of questions, options and the answer.
+# what have I done, and how I feel about it
+Adding the high score list from the localStorage was extremely challenging, and hung me up for quite a long time. I've managed to get it some semblance of functional, but I still struggled to add the localStorage object properties to a string that could then be looped to add to the High Scores list, so I've settled for just adding what is presently available in the LocalStorage but it lacks the ability to repopulate what was previously keyed if the window is refreshed.
+Otherwise, creating an array of questions for the quiz to pull from was probably the easiest part of this homework.
+I think the next time I approach something like this, it might be easier to just keep the different "sections" of the javascript in separate .js files so it wouldn't be such a struggle to remember all of the variables I was assigning...
+
+# what do I have left to do
+Other than fixing the High Scores list, I still need to add a timer to the quiz itself but grew pretty tired after struggling with the High Scores list functionality, so I haven't gotten there yet... but I'm going to try to add it asap!!
--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,31 @@
 
+
 // home page vars //
 const startContainer = document.getElementById("startContainer");
 const startButton = document.getElementById("startButton");
 const playAgainButton =  document.getElementById("playAgainButton");
 const highScoresButton =  document.getElementById("highScoresButton");
+
 // quiz //
 const quizContainer = document.getElementById("quizContainer");
 const optionA = document.getElementById("a");
 const optionB = document.getElementById("b");
 const optionC = document.getElementById("c");
 const optionD =  document.getElementById("d");
+
 // final score page //
 const answerResultContainer = document.getElementById("answerResultContainer");
 const scoreResult = document.getElementById("scoreResult");
 const scoreAmount = document.getElementById("scoreAmount");
-// score submission //
+
+// score submission and list //
+const highScoresList = document.getElementById("highScoresList");
+const highScoreContainer = document.getElementById("highScoreContainer");
 const username = document.getElementById("username");
 const saveScoreButton = document.getElementById("saveScoreButton");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-// high scores list page //
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-console.log(highScores);
-const highScoresList = document.getElementById("highScoresList");
-const highScoreContainer = document.getElementById("highScoreContainer");
-
+////////////////////////////////////
 
 // the questions //
 let questions = [
@@ -65,11 +66,11 @@ let questions = [
     answer : "d"
 }]
 
+
 // variables //
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 let score = 0;
-
 
 //rendering the question //
 function renderQuestion(){
@@ -87,8 +88,7 @@ function startQuiz (){
     startContainer.style.display = "none";
     renderQuestion();
     quizContainer.style.display = "block";
-    // start my timer function here when i make it //
-
+    var timeoutID;
 }
 
 //check answers//
@@ -121,40 +121,29 @@ function scoreCalculate(){
     // calculate and save the score //
     const scorePercent = Math.round(100 * score/questions.length);
     finalScore.innerText = scorePercent;
-    localStorage.setItem("mostRecentScore", scorePercent);
-
+    localStorage.setItem("userScore", scorePercent);
+    console.log(scorePercent);
 }
 
 //save score functions//
 username.addEventListener("keyup", () => {
-    saveScoreButton.disabled = !username.value;
+    saveScoreButton.disabled =! username.value;
 });
 
 saveHighScore = e => {
     e.preventDefault();
-    
-    const scoreTracker = {
-        name: username.value,
-        score: mostRecentScore
-    };
+        var userName = username.value;
+        localStorage.setItem("userName", userName);
+            userInfo = {
+            Name : localStorage.getItem("userName"),
+            Score: localStorage.getItem("userScore")
+            };
+    console.log(userName);
+    console.log(highScoresList);
 
-    highScores.push(scoreTracker);
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-    renderScores()
-    console.log(highScores);
-
+        highScoresList.innerHTML += userInfo.Name + ": " + userInfo.Score + "</br>";
 };
 
-highScoresList.innerHTML = highScores
-function renderScores() {
-    for (var i =0; i < highScores.length; i++) {
-        var newScoreAdded = highScores[i];
-
-        var li = document.createElement("li");
-        li.textContent = newScoreAdded;
-        highScoresList.appendChild(li);
-} 
-};
 
 
 highScoresButton.addEventListener("click", displayHighScores);
@@ -176,5 +165,4 @@ function startNewQuiz (){
     renderQuestion();
     quizContainer.style.display = "block";
     // start my timer function here when i make it //
-
-}
+};
